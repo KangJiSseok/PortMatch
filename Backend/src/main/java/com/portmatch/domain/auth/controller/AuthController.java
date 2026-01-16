2957bc033bafea9ead560c328ca6d9f0ac94e64e
KangJiSseok <rkdwltjr5876@naver.com>
2026-01-16 21:27:42 +0900

    feat: spring boot init & db 연동

---
+++ Backend/src/main/java/com/portmatch/domain/auth/controller/AuthController.java
@@ -0,0 +1,18 @@
+package com.portmatch.domain.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "Hello World";
+    }
+
+
+}
