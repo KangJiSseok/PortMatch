f5e41d694097c1c07a3374b8ba495186d067bcfb
강지석 <rkdwltjr5876@naver.com>
2026-01-21 14:31:32 +0900

    refactor: 클래스 병합 및 패키지 구조 변경

--- Backend/src/main/java/com/portmatch/domain/jobcompanies/repository/JobCompaniesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.portmatch.domain.jobcompanies.repository;
-
-import com.portmatch.domain.jobcompanies.entity.JobCompaniesEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobCompaniesRepository extends JpaRepository<JobCompaniesEntity, String> {
-}
