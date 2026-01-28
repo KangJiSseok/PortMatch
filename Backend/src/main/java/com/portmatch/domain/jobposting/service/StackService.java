dc04378ce10fa620c0a3f90002173aabe38fa140
조인예 <ye7484@gmail.com>
2026-01-28 09:15:39 +0900

    feat: stack create 추가 구현

--- Backend/src/main/java/com/portmatch/domain/jobposting/service/StackService.java
+++ Backend/src/main/java/com/portmatch/domain/jobposting/service/StackService.java
@@ -6,7 +6,7 @@ import com.portmatch.domain.jobposting.entity.PostingStackEntity;
 import java.util.List;
 
 public interface StackService {
-    void createStack(String s);
+    void createStack(Long id, String s);
 
     List<TechStackDto> getAllTechStacks();
 
