75cb54c422f7d9609dfa234603da65b57da54b6f
조인예 <ye7484@gmail.com>
2026-02-05 10:52:14 +0900

    fix: task 수정

--- Portfolio-Analysis/web/api/celery_app.py
+++ Portfolio-Analysis/web/api/celery_app.py
@@ -4,10 +4,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# 환경 변수 읽기 (Docker 환경에 맞게 기본값 설정)
+redis_host = os.getenv("REDIS_HOST", "portmatch-redis")
+redis_port = os.getenv("REDIS_PORT", "6379")
+redis_password = os.getenv("REDIS_PASSWORD", "")
+
+redis_url = f'redis://:{redis_password}@{redis_host}:{redis_port}/0'
+
 celery_app = Celery(
     'portmatch_analysis',
-    broker=f'redis://:{os.getenv("REDIS_PASSWORD")}@{os.getenv("REDIS_HOST", "portmatch-redis")}:{os.getenv("REDIS_PORT", "6379")}/0',
-    backend=f'redis://:{os.getenv("REDIS_PASSWORD")}@{os.getenv("REDIS_HOST", "portmatch-redis")}:{os.getenv("REDIS_PORT", "6379")}/0',
+    broker=redis_url,
+    backend=redis_url,
+    # ⭐ 파일명이 task.py(단수)라면 반드시 이렇게 써야 함!
     include=['web.api.task'] 
 )
 
@@ -19,6 +27,6 @@ celery_app.conf.update(
     enable_utc=True,
     task_track_started=True,
     task_time_limit=3600,
+    # 브로커 연결 리트라이 관련 경고 해결용
+    broker_connection_retry_on_startup=True 
 )
-
-celery_app.autodiscover_tasks(['web.api'])
