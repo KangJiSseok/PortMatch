c895dd992a4172f798cdc1303c276776d306361c
강지석 <rkdwltjr5876@naver.com>
2026-02-05 12:59:37 +0900

    refactor : 공고 지원 API swagger 필드 수정

--- Backend/src/main/java/com/portmatch/domain/jobapplication/dto/JobApplicationDetailResponse.java
+++ Backend/src/main/java/com/portmatch/domain/jobapplication/dto/JobApplicationDetailResponse.java
@@ -1,7 +1,9 @@
 package com.portmatch.domain.jobapplication.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.portmatch.domain.jobapplication.enums.ApplicationStatus;
 import com.portmatch.domain.resume.dto.ResumeResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -19,6 +21,8 @@ public class JobApplicationDetailResponse {
     private Long resumeId;
     private ApplicationStatus status;
     private LocalDateTime appliedAt;
+    @Schema(description = "Resume view status")
+    @JsonProperty("resumeViewed")
     private boolean resumeViewed;
     private ResumeResponse resume;
 }
