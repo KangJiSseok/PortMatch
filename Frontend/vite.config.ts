3dc4a6e33f04d839af601e16b8c2b7c6557e1603
jangjinuk <jjw3300@naver.com>
2026-02-07 22:07:12 +0900

    fix: env 파일 경로 수정

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const envDir = path.resolve(__dirname, '..');
+  const env = loadEnv(mode, envDir, '');
+
   const apiTarget = env.VITE_API_BASE_URL || 'http://i14d205.p.ssafy.io';
 
   return {
+    envDir,
     plugins: [
       react(),
       tailwindcss(),
